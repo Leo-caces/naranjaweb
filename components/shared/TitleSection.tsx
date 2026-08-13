9068a090635958f9b7aedc21024c10094c4794fb
Leo-caces <leo.caces@gmail.com>
2026-08-13 12:16:51 -0400

    titleSection fixes

--- components/shared/TitleSection.tsx
+++ components/shared/TitleSection.tsx
@@ -9,11 +9,11 @@ interface Props {
 
 export const TitleSection = ({title, highlighted, description, className}: Props) => {
   return <div className="flex flex-col items-center justify-center">
-          <h2 className={cn('text-[45px] font-semibold text-background', className)}>
+          <h2 className={cn('text-4xl font-semibold text-background', className)}>
             {title} 
-            <span className={cn('text-[45px] font-semibold text-primary ')}> {highlighted}</span>
+            <span className={cn('text-4xl font-semibold text-primary')}> {highlighted}</span>
           </h2>
           <div className="h-0.5 bg-[#ffd43b] w-21 mt-1"></div>
-          <p className="text-fontGray text-[18px] mt-4 text-center px-4">{description}</p>
+          <p className="text-fontGray text-base mt-4 mx-auto text-center px-4">{description}</p>
         </div>
 }
